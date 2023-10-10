9cf98bbabdd3575853850fbfd00aab45adb9ee3a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-10 11:44:19 -0300

    bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import TextSize from "./componentes/TextSize"
+import TextPerson from "./componentes/TextPerson"
+import TextColor from "./componentes/TextColor"
+import TextBackground from "./componentes/TextBackground"
+import Imagem from "./componentes/Imagem"
+function App() {
+  
+
+  return (
+    <>
+      <div className="container">
+        <TextSize/>
+        <TextPerson/>
+        <TextColor/>
+        <TextBackground/>
+        <Imagem/>
+      </div>
+    </>
+  )
+}
+
+export default App
